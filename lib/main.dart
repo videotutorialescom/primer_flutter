c808fa3a9e38bd8e0a258294a3d6aa5aab29ee4b
0utKast <educaleft@gmail.com>
2019-02-08 16:22:19 +0000

    Añadida ListView

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,68 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
+      title: 'Generador de Nombres para Startups',
+      home: RandomWords(),
+    );
+  }
+  // #enddocregion build
+}
+// #enddocregion MyApp
+
+// #docregion RWS-var
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
-}
+  // #enddocregion _buildRow
 
-// #docregion RandomWordsState, RWS-class-only
-class RandomWordsState extends State<RandomWords> {
-  // #enddocregion RWS-class-only
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Generador de Nombres para Startups'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
-  // #docregion RWS-class-only
+  // #enddocregion RWS-build
+  // #docregion RWS-var
 }
-// #enddocregion RandomWordsState, RWS-class-only
+// #enddocregion RWS-var
 
-// #docregion RandomWords
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
